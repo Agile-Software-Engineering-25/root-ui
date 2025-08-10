3e5659f834de5ed238b4aa708cae05b57b879757
Janne <jabbekeipert@gmail.com>
2025-08-11 01:17:00 +0200

    initial setup

---
+++ src/apps.ts
@@ -0,0 +1,8 @@
+/** this is a list of all applications (micro-services) that are accessible by the root config */
+export default [
+  {
+    name: "@agile-software-engineering/frontend-template",
+    activeWhen: "/",
+    mountWhere: "#frontend-template",
+  },
+];
---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+// pkg
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { registerApplication, start, getAppNames } from "single-spa";
+import apps from "./apps";
+
+apps.forEach((app) => {
+  registerApplication({
+    name: app.name,
+    activeWhen: app.activeWhen,
+    customProps: {
+      domElement: document.querySelector(app.mountWhere),
+    },
+    app: () => import(/* @vite-ignore */ app.name),
+  });
+});
+
+if (process.env.NODE_ENV === "development") {
+  console.log("APPLICATIONS", getAppNames());
+  // enable the single spa import map override panel in dev mode
+  localStorage.setItem("imo-ui", "true");
+} else {
+  // disable the single spa import map override panel for built environments (can still be accessed using the browser extension)
+  localStorage.setItem("imo-ui", "false");
+}
+start();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <nav>Navigations Bar</nav>
+    <div id="single-spa-application:@agile-software-engineering/frontend-template"></div>
+    <footer>Footer</footer>
+  </StrictMode>
+);
