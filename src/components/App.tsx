7b3e79e529174d5a4902709727f097863ae0f594
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2025-09-05 16:07:46 +0200

    Moved ui structure to skeleton file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,43 +1,12 @@
 import { useAuth } from "react-oidc-context";
-import EmbeddedApplication from "./EmbeddedApplication/EmbeddedApplication";
+import Skeleton from "./Skeleton";
 
 /* dont come at me for this design - its 2 am and I just want to get this working */
 const App = () => {
     const auth = useAuth();
     
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        maxWidth: "100%",
-      }}
-    >
-      <nav
-        style={{
-          backgroundColor: "lightblue",
-          padding: "1rem",
-          width: "calc(100% - 2rem)",
-        }}
-      >
-        Navigations Bar
-      </nav>
-      <EmbeddedApplication
-        name="@agile-software-engineering/frontend-template"
-        sx={{ flexGrow: 1 }}
-        userData={auth.user}
-      />
-      <footer
-        style={{
-          backgroundColor: "lightgray",
-          padding: "1rem",
-          width: "calc(100% - 2rem)",
-        }}
-      >
-        Footer
-      </footer>
-    </div>
+    <Skeleton/>
   );
 };
 
---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,44 @@
+import { useAuth } from "react-oidc-context";
+import EmbeddedApplication from "./EmbeddedApplication/EmbeddedApplication";
+
+/* dont come at me for this design - its 2 am and I just want to get this working */
+const Skeleton = () => {
+  const auth = useAuth();
+    
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        maxWidth: "100%",
+      }}
+    >
+      <nav
+        style={{
+          backgroundColor: "lightblue",
+          padding: "1rem",
+          width: "calc(100% - 2rem)",
+        }}
+      >
+        Navigations Bar
+      </nav>
+      <EmbeddedApplication
+        name="@agile-software-engineering/frontend-template"
+        sx={{ flexGrow: 1 }}
+        userData={auth.user}
+      />
+      <footer
+        style={{
+          backgroundColor: "lightgray",
+          padding: "1rem",
+          width: "calc(100% - 2rem)",
+        }}
+      >
+        Footer
+      </footer>
+    </div>
+  );
+};
+
+export default Skeleton;
