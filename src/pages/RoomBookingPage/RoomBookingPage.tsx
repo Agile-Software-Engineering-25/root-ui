450b257a94266196a5269a62d9492cadf0986e30
Janne Keipert <janne.keipert@telekom.de>
2025-09-10 12:33:20 +0200

    update shared components

--- src/pages/RoomBookingPage/RoomBookingPage.tsx
+++ src/pages/RoomBookingPage/RoomBookingPage.tsx
@@ -1,23 +1,11 @@
-import { Box, Typography } from "@mui/joy";
+import EmbeddedApplication from "../../components/EmbeddedApplication/EmbeddedApplication";
 
 const RoomBookingService = () => {
   return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Typography level={"title-md"} fontSize={"64px"} component={"h1"}>
-        Willkommen im neuen Hochschulportal
-      </Typography>
-      <Typography level={"title-md"} fontSize={"36px"} component={"h2"}>
-        Sieh dir die Funktionen an:
-      </Typography>
-    </Box>
+    <EmbeddedApplication
+      name="@agile-software-engineering/ase-01-room-booking-service"
+      sx={{ flexGrow: 1 }}
+    />
   );
 };
 
